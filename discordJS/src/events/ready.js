bcbfce918e590ad8b6c44a2e7fbdbf87cd32c282
Restyled.io <commits@restyled.io>
2023-02-28 08:42:40 +0000

    Restyled by clang-format

--- discordJS/src/events/ready.js
+++ discordJS/src/events/ready.js
@@ -1,9 +1,8 @@
-const { Events } = require('discord.js');
+const {Events} = require('discord.js');
 
 module.exports = {
-	name: 'clientReady',
-	once: true,
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
+  name : 'clientReady',
+  once : true,
+       execute(
+           client) { console.log(`Ready! Logged in as ${client.user.tag}`); },
 };
--- discordJS/src/main.js
+++ discordJS/src/main.js
@@ -1,22 +1,24 @@
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const {Client, GatewayIntentBits, Collection} = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const client = new Client(
+    {intents : [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages ]});
 
 client.commands = new Collection();
 
 const fs = require('fs');
 
-require('dotenv').config({path:__dirname+'/../.env'});
+require('dotenv').config({path : __dirname + '/../.env'});
 
-const functions = fs.readdirSync('./functions').filter(file => file.endsWith('.js'));
+const functions =
+    fs.readdirSync('./functions').filter(file => file.endsWith('.js'));
 const commandFolders = fs.readdirSync('./commands');
 const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
 (async () => {
-    for (file of functions){
-        require(`./functions/${file}`)(client);
-    }
-    client.handleEvents(events, "./src/events");
-    client.handleCommands(commandFolders, "./commands");
-    client.login(process.env.TOKEN);
+  for (file of functions) {
+    require(`./functions/${file}`)(client);
+  }
+  client.handleEvents(events, "./src/events");
+  client.handleCommands(commandFolders, "./commands");
+  client.login(process.env.TOKEN);
 })();
